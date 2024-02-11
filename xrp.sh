e322fe7cafb4b2d640657150114e4c10dbb0e0af
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-11 17:06:37 +0700

    Update xrp.sh

--- xrp.sh
+++ xrp.sh
@@ -10,13 +10,13 @@ tar -xf SRBMiner-Multi-2-4-7-Linux.tar.xz
 cd SRBMiner-Multi-2-4-7 || exit
 
 # Menjalankan SRBMiner-Multi untuk 4 instance
-for ((i = 1; i <= 4; i++)); do
-    screen -dmS SRBMiner$i ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i
-    echo "Berhasil menjalankan SRBMiner ke-$i"
-done
+# for ((i = 1; i <= 4; i++)); do
+#     screen -dmS SRBMiner$i ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i
+#     echo "Berhasil menjalankan SRBMiner ke-$i"
+# done
 
 # Menampilkan daftar layar
-screen -ls
+# screen -ls
 
 # Menjalankan SRBMiner-MULTI untuk instance ke-5
 ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-5
