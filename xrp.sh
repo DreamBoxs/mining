978cb2bf1b3fe5eabdb22ee882719f4eb59b5ae3
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-11 12:33:47 +0700

    Create xrp.sh

---
+++ xrp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Mengunduh SRBMiner-Multi
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
+
+# Mengekstrak file yang diunduh
+tar -xf SRBMiner-Multi-2-4-7-Linux.tar.xz
+
+# Masuk ke direktori SRBMiner-Multi-2-4-7 atau keluar jika gagal
+cd SRBMiner-Multi-2-4-7 || exit
+
+# Menjalankan SRBMiner-Multi untuk 4 instance
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS SRBMiner$i ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i
+    echo "Berhasil menjalankan SRBMiner ke-$i"
+done
+
+# Menampilkan daftar layar
+screen -ls
+
+# Menjalankan SRBMiner-MULTI untuk instance ke-5
+./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-5
