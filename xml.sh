c7d1189abb83fc77d4ef09dea9cd0b83762b241a
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-11 17:07:20 +0700

    Update xml.sh

--- xml.sh
+++ xml.sh
@@ -10,13 +10,13 @@ tar -xf SRBMiner-Multi-2-4-7-Linux.tar.xz
 cd SRBMiner-Multi-2-4-7 || exit
 
 # Menjalankan SRBMiner-Multi untuk 4 instance
-for ((i = 1; i <= 4; i++)); do
-    screen -dmS SRBMiner$i ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i
-    echo "Berhasil menjalankan SRBMiner ke-$i"
-done
+# for ((i = 1; i <= 4; i++)); do
+#     screen -dmS SRBMiner$i ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i
+#     echo "Berhasil menjalankan SRBMiner ke-$i"
+# done
 
 # Menampilkan daftar layar
-screen -ls
+# screen -ls
 
 # Menjalankan SRBMiner-MULTI untuk instance ke-5
 ./SRBMiner-MULTI --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-5
