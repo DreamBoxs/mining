218a7ea5ad2bc62d8d437d72af5e98036a347510
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-12 09:42:32 +0700

    Update xml.sh

--- xml.sh
+++ xml.sh
@@ -11,7 +11,7 @@ cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
 # for ((i = 1; i <= 4; i++)); do
-#     screen -dmS SRBMiner$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.xml-$i -p x 
+#     screen -dmS SRBMiner$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i -p x 
 #     echo "Berhasil menjalankan SRBMiner ke-$i"
 # done
 
@@ -19,4 +19,4 @@ cd xmrig-6.21.0 || exit
 # screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.xml-5 -p x
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-5 -p x
