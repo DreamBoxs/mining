a1f676d9003040e0852d7bed74eaeeea3563020d
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-13 18:05:23 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -10,10 +10,10 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS SRBMiner$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i.rig-$i -p x 
-#     echo "Berhasil menjalankan SRBMiner ke-$i"
-# done
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS XMR$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i.rig-$i -p x 
+    echo "Berhasil menjalankan xmr ke-$i"
+done
 
 # Menampilkan daftar layar
 # screen -ls
