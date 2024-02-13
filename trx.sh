258a6daaed9597df34df6fa11d4ec62e25d79643
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-13 18:06:35 +0700

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -10,10 +10,10 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS SRBMiner$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.rig-$i -p x 
-#     echo "Berhasil menjalankan SRBMiner ke-$i"
-# done
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS TRX$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.rig-$i -p x 
+    echo "Berhasil menjalankan trx ke-$i"
+done
 
 # Menampilkan daftar layar
 # screen -ls
