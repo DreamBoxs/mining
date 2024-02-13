cd2efe1757c948dc7ef029d659d72efb23cccc4d
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-13 18:14:57 +0700

    Update xrp.sh

--- xrp.sh
+++ xrp.sh
@@ -11,7 +11,7 @@ cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
 for ((i = 1; i <= 4; i++)); do
-    screen -dmS XRP$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i -p x 
+    screen -dmS XRP$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i -p x 
     echo "Berhasil menjalankan xrp ke-$i"
 done
 
