67f420af395aa0b345ead45e6b84ed8b80468b6e
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-13 18:04:26 +0700

    Update xrp.sh

--- xrp.sh
+++ xrp.sh
@@ -10,13 +10,13 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS SRBMiner$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i -p x 
-#     echo "Berhasil menjalankan SRBMiner ke-$i"
-# done
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS XRP$i ../xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-$i -p x 
+    echo "Berhasil menjalankan xrp ke-$i"
+done
 
 # Menampilkan daftar layar
-# screen -ls
+screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XRP:rPmk8dPsixqvfP8mUy2iiGCy2zs76hr8ng.rig-5 -p x 
