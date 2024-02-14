1015bb037a3ccaba864a26051ebc23d635d924c6
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-14 08:16:01 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -10,13 +10,13 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS XRP$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:LKff4nHW6UkdKFj6SruixG4cfwUngUHyrz.rig-$i -p x 
-#     echo "Berhasil menjalankan xrp ke-$i"
-# done
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS XRP$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:LKff4nHW6UkdKFj6SruixG4cfwUngUHyrz.rig-$i -p x 
+    echo "Berhasil menjalankan xrp ke-$i"
+done
 
 # Menampilkan daftar layar
-# screen -ls
+screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:LKff4nHW6UkdKFj6SruixG4cfwUngUHyrz.rig-5 -p x 
