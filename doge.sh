200a89922ae59954a5ca0b5331d1dc77c496d93b
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-15 01:26:04 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -9,14 +9,14 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 # Masuk ke direktori xmrig-6.21.0 atau keluar jika gagal
 cd xmrig-6.21.0 || exit
 
-# Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS DOGE$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XDOGE:DQPFq8Zzp89HAXxeM6LY1ya4hsy2vV5BSY.rig-$i -p x 
-#     echo "Berhasil menjalankan doge ke-$i"
-# done
+#"Menjalankan xmrig untuk 4 instance
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS DOGE$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XDOGE:DQPFq8Zzp89HAXxeM6LY1ya4hsy2vV5BSY.rig-$i -p x 
+    echo "Berhasil menjalankan doge ke-$i"
+done
 
 # Menampilkan daftar layar
-# screen -ls
+screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:DQPFq8Zzp89HAXxeM6LY1ya4hsy2vV5BSY.rig-5 -p x 
