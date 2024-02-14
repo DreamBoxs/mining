ad73dba7f2b602997d418cddb0bddb413ec06a86
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-14 07:27:54 +0700

    Update xlm.sh

--- xlm.sh
+++ xlm.sh
@@ -16,7 +16,7 @@ cd xmrig-6.21.0 || exit
 # done
 
 # Menampilkan daftar layar
-screen -ls
+# screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-5 -p x
