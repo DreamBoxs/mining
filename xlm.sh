d652e995325895d8ac2627680dc4691bb006a823
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-15 01:27:20 +0700

    Update xlm.sh

--- xlm.sh
+++ xlm.sh
@@ -10,13 +10,13 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0 || exit
 
 # Menjalankan xmrig untuk 4 instance
-# for ((i = 1; i <= 4; i++)); do
-#     screen -dmS XLM$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i -p x 
-#     echo "Berhasil menjalankan mining xlm ke-$i"
-# done
+for ((i = 1; i <= 4; i++)); do
+    screen -dmS XLM$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i -p x 
+    echo "Berhasil menjalankan mining xlm ke-$i"
+done
 
 # Menampilkan daftar layar
-# screen -ls
+screen -ls
 
 # Menjalankan xmrig untuk instance ke-5
 ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-5 -p x
