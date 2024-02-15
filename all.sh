52a02b8fb0d1050e3b2100cf4072098bd46d4d69
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-02-16 00:50:37 +0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -16,7 +16,9 @@ declare -A running_list=(
 
 for coin in "${!running_list[@]}"; do
     screen -dmS "xmrig_$coin" ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u "$coin:${running_list[$coin]}.$coin" -p x 
-    echo "Berhasil menjalankan xmrig untuk $coin"
+    echo "-------------------------------------------------------------------"
+    echo "|                       MINING $coin STARTED                      |"
+    echo "-------------------------------------------------------------------"
 done
 
 sleep infinity 
