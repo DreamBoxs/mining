2d5bb96a717d97feee5e9a71529638af166583a4
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-03 14:59:09 +0700

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Mengunduh xmrig 
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
 
-# Mengekstrak file yang diunduh
 tar -xf xmrig-6.21.0-linux-x64.tar.gz
 
-# Masuk ke direktori xmrig-6.21.0 atau keluar jika gagal
-cd xmrig-6.21.0 || exit
+cd xmrig-6.21.0
 
-# Menjalankan xmrig untuk 4 instance
-for ((i = 1; i <= 4; i++)); do
-    screen -dmS TRX$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.rig-$i -p x 
-    echo "Berhasil menjalankan trx ke-$i"
-done
-
-# Menampilkan daftar layar
-screen -ls
-
-# Menjalankan xmrig untuk instance ke-5
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.rig-5 -p x 
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.TRX -p x 
