c72880786b8d34d722aceca9a971124c5d87cea1
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-03 14:15:57 +0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -9,10 +9,20 @@ declare -A running_list=(
     [LTC]=LKff4nHW6UkdKFj6SruixG4cfwUngUHyrz
     [TRX]=TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj
     [XLM]=GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS
+    [XMR]=47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i
 )
 
 for coin in "${!running_list[@]}"; do
     screen -dmS "xmrig_$coin" ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u "$coin:${running_list[$coin]}.$coin" -p x 
 done
 
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i.RIG-1 -p x
+echo "╔═╗╔═╗───────────╔═╗╔═╗─────────╔═══╗╔╗─────╔╗─────╔╗"
+echo "║║╚╝║║───────────╚╗╚╝╔╝─────────║╔═╗╠╝╚╗───╔╝╚╗────║║"
+echo "║╔╗╔╗╠╦═╗╔╦═╗╔══╗─╚╗╔╝╔╗╔╦═╦╦══╗║╚══╬╗╔╬══╦╩╗╔╬══╦═╝║"
+echo "║║║║║╠╣╔╗╬╣╔╗╣╔╗║─╔╝╚╗║╚╝║╔╬╣╔╗║╚══╗║║║║╔╗║╔╣║║║═╣╔╗║"
+echo "║║║║║║║║║║║║║║╚╝║╔╝╔╗╚╣║║║║║║╚╝║║╚═╝║║╚╣╔╗║║║╚╣║═╣╚╝║"
+echo "╚╝╚╝╚╩╩╝╚╩╩╝╚╩═╗║╚═╝╚═╩╩╩╩╝╚╩═╗║╚═══╝╚═╩╝╚╩╝╚═╩══╩══╝"
+echo "─────────────╔═╝║───────────╔═╝║"
+echo "─────────────╚══╝───────────╚══╝"
+
+sleep infinity
