5709ff30e2ca5c7b7d55162563d5069dd9368798
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-03 15:01:12 +0700

    Update xlm.sh

--- xlm.sh
+++ xlm.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Mengunduh xmrig 
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
 
-# Mengekstrak file yang diunduh
 tar -xf xmrig-6.21.0-linux-x64.tar.gz
 
-# Masuk ke direktori xmrig-6.21.0 atau keluar jika gagal
-cd xmrig-6.21.0 || exit
+cd xmrig-6.21.0
 
-# Menjalankan xmrig untuk 4 instance
-for ((i = 1; i <= 4; i++)); do
-    screen -dmS XLM$i ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-$i -p x 
-    echo "Berhasil menjalankan mining xlm ke-$i"
-done
-
-# Menampilkan daftar layar
-screen -ls
-
-# Menjalankan xmrig untuk instance ke-5
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.rig-5 -p x
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.XLM -p x
