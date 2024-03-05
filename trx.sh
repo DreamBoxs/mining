1b7040940df332c1891a59ef4b0ca319d4f1dbb9
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-06 04:14:25 +0700

    Update trx.sh

--- trx.sh
+++ trx.sh
@@ -7,3 +7,5 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0
 
 screen -dmS TRX ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TMLSJnPCRvshn1aV5zmdg2zWwg5LKgUAAj.TRX -p x 
+
+sleep 
