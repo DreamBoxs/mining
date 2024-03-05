a19365d104c1af222e869c91ec72caa20c77c700
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-06 04:12:03 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -6,4 +6,6 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 
 cd xmrig-6.21.0
 
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:DQPFq8Zzp89HAXxeM6LY1ya4hsy2vV5BSY.DOGE -p x 
+screen -dmS DOGE ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:DQPFq8Zzp89HAXxeM6LY1ya4hsy2vV5BSY.DOGE -p x 
+
+sleep infinity 
