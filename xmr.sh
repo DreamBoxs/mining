f8a88f7d5a3f2e785f497ac4ff17cb29081a433f
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-06 04:32:12 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -6,6 +6,12 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 
 cd xmrig-6.21.0
 
-screen -dmS XMR ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i.XMR-p x 
+screen -dmS XMR ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:47bE71spiv7d7Pr7iY9yuQ4xyNK921fDtQiVqoNTrWvtU5JxAEThCGUGcMTm2NoUrfjDb1EGqMsdFbwwrdCEBfcY3zfBk9i.XMR-p x
 
-sleep infinity 
+echo "╔═╦═╦╗──╔╗─────╔╗╔╗────╔╗──╔══╦╗─────╔╗───╔╗"
+echo "║║║║╠╬═╦╬╬═╦╦═╗╚╗╔╬══╦╦╬╬═╗║══╣╚╦═╗╔╦╣╚╦═╦╝║"
+echo "║║║║║║║║║║║║║╬║╔╝╚╣║║║╔╣║╬║╠══║╔╣╬╚╣╔╣╔╣╩╣╬║"
+echo "╚╩═╩╩╩╩═╩╩╩═╬╗║╚╝╚╩╩╩╩╝╚╬╗║╚══╩═╩══╩╝╚═╩═╩═╝"
+echo "────────────╚═╝─────────╚═╝"
+
+sleep infinity
