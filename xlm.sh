b6cb3ad72b0b7a14c758adde7f569ac55b9840ac
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-06 04:15:33 +0700

    Update xlm.sh

--- xlm.sh
+++ xlm.sh
@@ -6,4 +6,4 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 
 cd xmrig-6.21.0
 
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.XLM -p x
+screen -dmS XLM ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.XLM -p x
