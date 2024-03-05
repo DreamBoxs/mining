9506ff9c33257c98da9c81a60b1486f5d6eb594b
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-06 04:31:13 +0700

    Update xlm.sh

--- xlm.sh
+++ xlm.sh
@@ -8,4 +8,10 @@ cd xmrig-6.21.0
 
 screen -dmS XLM ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XLM:GB2NT6CIX4X2BQI4D4I3RIV2G4J2D5KEKHAPPV66KK4NQ6MHLGSDSWXS.XLM -p x
 
-sleep infinity 
+echo "╔═╦═╦╗──╔╗─────╔╗╔╗────╔╗──╔══╦╗─────╔╗───╔╗"
+echo "║║║║╠╬═╦╬╬═╦╦═╗╚╗╔╬══╦╦╬╬═╗║══╣╚╦═╗╔╦╣╚╦═╦╝║"
+echo "║║║║║║║║║║║║║╬║╔╝╚╣║║║╔╣║╬║╠══║╔╣╬╚╣╔╣╔╣╩╣╬║"
+echo "╚╩═╩╩╩╩═╩╩╩═╬╗║╚╝╚╩╩╩╩╝╚╬╗║╚══╩═╩══╩╝╚═╩═╩═╝"
+echo "────────────╚═╝─────────╚═╝"
+
+sleep infinity
