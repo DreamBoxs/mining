d95c9e7d5c094555c5cdabff671709ba693503a8
ᴘʀᴏᴊᴇᴄᴛs ɪɴᴅᴏɴᴇsɪᴀ <159602802+DreamBoxs@users.noreply.github.com>
2024-03-14 01:58:04 +0700

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
 
 tar -xf xmrig-6.21.0-linux-x64.tar.gz
@@ -5,11 +7,5 @@ tar -xf xmrig-6.21.0-linux-x64.tar.gz
 cd xmrig-6.21.0
 
 screen -dmS LTC ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:LKff4nHW6UkdKFj6SruixG4cfwUngUHyrz.LTC -p x
-
-echo "╔═╦═╦╗──╔╗─────╔╗╔╗────╔╗──╔══╦╗─────╔╗───╔╗"
-echo "║║║║╠╬═╦╬╬═╦╦═╗╚╗╔╬══╦╦╬╬═╗║══╣╚╦═╗╔╦╣╚╦═╦╝║"
-echo "║║║║║║║║║║║║║╬║╔╝╚╣║║║╔╣║╬║╠══║╔╣╬╚╣╔╣╔╣╩╣╬║"
-echo "╚╩═╩╩╩╩═╩╩╩═╬╗║╚╝╚╩╩╩╩╝╚╬╗║╚══╩═╩══╩╝╚═╩═╩═╝"
-echo "────────────╚═╝─────────╚═╝"
 
 sleep infinity
